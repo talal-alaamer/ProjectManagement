d34823a2d1b86d1f550bcc3a6f377381c724952c
Administrator <Administrator@DESKTOP-BSUB3KC>
2023-06-01 16:45:15 +0100

    get users count

--- ProjectManagement/Controllers/HomeController.cs
+++ ProjectManagement/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using ProjectManagement.Areas.Identity.Data;
+using ProjectManagement.Data;
 using ProjectManagement.Model;
 using System.Diagnostics;
 
@@ -6,15 +9,28 @@ namespace ProjectManagement.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly UserManager<Users> _userManager;
+        private readonly SignInManager<Users> _signInManager;
+        private readonly IdentityContext _context;
+
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(UserManager<Users> userManager, SignInManager<Users> signInManager, IdentityContext context)
         {
-            _logger = logger;
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _context = context;
         }
+
+        //public HomeController(ILogger<HomeController> logger)
+        //{
+        //    _logger = logger;
+        //}
         
         public IActionResult Index()
         {
+            int totalUsers = _context.Users.Count();
+            ViewData["TotalUsers"] = totalUsers;
             return View();
         }
 
